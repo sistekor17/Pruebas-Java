84f40c0aafd7b6e95e6da9b3172ffcefab5c9dbb
sistekor17 <jamesserrato17@gmail.com>
2026-02-27 20:57:23 -0300

    primer commit

---
+++ src/main/java/com/prueba/demo/PruebaPersonaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaPersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaPersonaApplication.class, args);
+	}
+
+}
